ad593bb0362ea77a9780b46a1eeb702ea437d64a
mohamedali abbes <mohamedali.abbes@gmail.com>
2023-07-06 11:02:55 -0400

    added contoller forlder

---
+++ controllers/user.controller.js
@@ -0,0 +1,25 @@
+
+
+/**
+ * Controller should parse the request data and pass objects to the service
+ * 
+ */
+
+const Create = (req , res) => {
+    
+}
+
+const Update = (req , res) => {
+    
+}
+
+const Delete = (req , res) => {
+    
+}
+
+const Read = (req , res) => {
+    
+}
+
+
+module.exports = { Create , Update , Delete , Read } ;
