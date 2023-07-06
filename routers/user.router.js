ddf0d30914db8000ac728a5176a9bd812ea9e1ee
mohamedali abbes <mohamedali.abbes@gmail.com>
2023-07-06 11:13:47 -0400

    added router structure

---
+++ routers/user.router.js
@@ -0,0 +1,25 @@
+
+
+const router               = require('express').Router() ;
+const UserController       = require('./../controllers/user.controller');
+/**
+ * Example on how this should work :
+ * the request will reashes the router where it will passed to the middleware to check 
+ * authorization ,if it is authorized it will be passed to the controller where the logic 
+ * will happen if not an error will be return to the client 
+ * 
+ * router.method('/path' , middleware ,controller) ; 
+ */
+
+router.post('/' , ) ;
+
+router.get('/'  , ) ;
+
+router.get('/:id' ,) ;
+
+router.put('/:id' , ) ;
+
+router.delete('/:id' , ) ;
+
+
+module.exports = router ;
